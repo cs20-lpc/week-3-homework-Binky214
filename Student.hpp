54f63bce8f2e9130469f153060c9b565f2d47105
Binky214 <39577742+Binky214@users.noreply.github.com>
2026-02-09 16:46:35 -0800

    Define Student class with member variables and methods

--- Student.hpp
+++ Student.hpp
@@ -1,1 +1,23 @@
-// TO DO:  Student class definition  here.
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <string>
+#include "Course.hpp"
+
+class Student {
+private:
+    int id;
+    std::string name;
+    double gpa;
+    Course* courseHead;
+
+public:
+    Student(int id, const std::string& name, double gpa);
+    ~Student();
+
+    int getId() const;
+    void addCourse(const std::string& name, const std::string& location);
+    void display() const;
+};
+
+#endif
