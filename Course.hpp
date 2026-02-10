1bb74c7f7029d50a0f71174509cea030256e85f9
Binky214 <39577742+Binky214@users.noreply.github.com>
2026-02-09 16:47:19 -0800

    Define Course class with name and location attributes

--- Course.hpp
+++ Course.hpp
@@ -1,1 +1,15 @@
-// TODO:  Course class definitins go here.
+#ifndef COURSE_HPP
+#define COURSE_HPP
+
+#include <string>
+
+class Course {
+public:
+    std::string name;
+    std::string location;
+    Course* next;
+
+    Course(const std::string& name, const std::string& location);
+};
+
+#endif
