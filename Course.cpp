533bfff2528c704e8a3952d141452cebc6c8efbe
Binky214 <39577742+Binky214@users.noreply.github.com>
2026-02-09 16:47:35 -0800

    Implement Course constructor in Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,1 +1,4 @@
-// TODO:  Course class methods implementation here.
+#include "Course.hpp"
+
+Course::Course(const std::string& name, const std::string& location)
+    : name(name), location(location), next(nullptr) {}
